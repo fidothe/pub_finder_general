0bd0be42783ceb28260160047176fa8c2ba67fd7
Lauren Wood <lauren@textuality.com>
2011-09-20 06:19:21 -0700

    removed test data added h3 elements to pages for consistent styling

--- app.rb
+++ app.rb
@@ -52,23 +52,6 @@ DataMapper.auto_migrate! # Warning - this will wipe out any existing data in tab
 # DataMapper.auto_upgrade! # Will create new tables, and add columns where needed. 
                            # It won't change column constraints or drop columns
 
-# add some stuff for testing
-@pub = Pub.create(
-  :name => "white",
-  :description => "another pub"
-)
-
-@reviewer = Reviewer.create(
-  :twitterid => "laurendw",
-  :name => "my name"
-)
-
-@review = Review.create(
-  :pub_id => 1,
-  :reviewer_id => 1,
-  :text => "my first review of the white pub"
-)
-
 # Now the twitter stuff, filling in CONSUMER_KEY and CONSUMER_SECRET
 
 use OmniAuth::Strategies::Twitter, 'IZXyIwBrkrV9KJXHkYo4HQ', 'dbbAWBA3V52ExlImrYQ9BXGDebcIt4vZQ30C55pNs'
@@ -131,7 +114,6 @@ end
 get '/reviewers/new' do
   @title = "Add a reviewer"
   redirect '/auth/twitter'
-#  erubis :'reviewers/new'
 end
 
 post '/reviewers' do
