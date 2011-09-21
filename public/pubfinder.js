f53fb1270bdbaa8e3aba0f8c5dab1d714f3a6e1b
Norman Walsh <ndw@nwalsh.com>
2011-09-21 16:13:15 +0100

    Added support for geolocation

--- public/pubfinder.js
+++ public/pubfinder.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
     if (datetest.type == "text") {
         $("#review_date").datepicker({ dateFormat: "yy-mm-dd" });
     }
+
+    if ($("form.newpub").size() > 0) {
+        if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(
+                updateGeo, handleError);
+        }
+    }
 });
+
+function updateGeo(position) {
+    $("form.newpub").append("<input type='hidden' name='pub[lat]' value='" + position.coords.latitude + "' />");
+    $("form.newpub").append("<input type='hidden' name='pub[lon]' value='" + position.coords.longitude + "' />");
+    $("#geowait").html("Found geolocation!");
+}
+
+function handleError(err) {
+    alert ('Geolocation failed');
+}
